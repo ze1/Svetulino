d914a60f7bf05b8eb369897db3a8234d62c97ce1
ao <alder123>
2016-06-07 04:45:39 +0300

    Add project files.

---
+++ Svetulino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Svetulino.ino
+ Created:	2016-06-07 04:45:38
+ Author:	alder_000
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
